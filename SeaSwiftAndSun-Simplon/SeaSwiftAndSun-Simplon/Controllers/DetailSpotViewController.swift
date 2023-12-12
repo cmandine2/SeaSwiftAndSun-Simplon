cc86617adc6f2e4d22aeb914c4a35086ef4f4739
z888e <zoemhrt@gmail.com>
2023-12-12 13:41:21 +0100

    prep + livecoding collectif

--- SeaSwiftAndSun-Simplon/SeaSwiftAndSun-Simplon/Controllers/DetailSpotViewController.swift
+++ SeaSwiftAndSun-Simplon/SeaSwiftAndSun-Simplon/Controllers/DetailSpotViewController.swift
@@ -10,14 +10,30 @@ import UIKit
 class DetailSpotViewController: UIViewController {
     @IBOutlet weak var spotImage: UIImageView!
     @IBOutlet weak var spotName: UILabel!
-    
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        
-        self.setUpUI()
-    }
-    
+	var spotDetails: SurfSpotFields?
+	
+	override func viewDidLoad() {
+		super.viewDidLoad()
+		print("details loaded")
+		self.setUpUI()
+		print("details set up")
+	}
+
     func setUpUI() {
-        self.spotImage.layer.cornerRadius = self.spotImage.frame.size.width / 2
+		if let details = spotDetails {
+					if let photos = details.photos {
+						if let thumbnailUrl = photos.first?.url,
+						   let url = URL(string: thumbnailUrl) {
+							if let spotImage = self.spotImage {
+								self.spotImage.load(url: url)
+								self.spotImage.layer.cornerRadius = self.spotImage.frame.size.width / 2
+							}
+						} else {
+							self.spotImage.image = UIImage(named: "surfSpot") // Image par défaut
+						}
+					}
+			
+			self.spotName.text = details.destination
+		}
     }
 }
--- SeaSwiftAndSun-Simplon/SeaSwiftAndSun-Simplon/Controllers/ViewController.swift
+++ SeaSwiftAndSun-Simplon/SeaSwiftAndSun-Simplon/Controllers/ViewController.swift
@@ -8,41 +8,73 @@
 import UIKit
 
 class ViewController: UIViewController {
-    
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        
-        self.title = "Liste des spots de surf"
-    }
-}
+	@IBOutlet weak var tableView: UITableView!
+	var surfSpots: [SurfSpotFields] = []
 
+	override func viewDidLoad() {
+		super.viewDidLoad()
+		tableView.delegate = self
+		tableView.dataSource = self
+		loadSurfSpotsData()
+	}
+	
+	private func loadSurfSpotsData() {
+		NetworkManager.shared.fetchSurfSpots { [weak self] result in
+			DispatchQueue.main.async {
+				switch result {
+					case .success(let surfSpotResponse):
+//						print("success test")
+						self?.surfSpots = surfSpotResponse.records.map { $0.fields }
+						self?.updateUI()
+					case .failure(let error):
+						print("failure test")
+						self?.handleError(error)
+				}
+			}
+		}
+	}
+	
+	private func updateUI() {
+		tableView.reloadData()
+	}
+	
+	private func handleError(_ error: Error) {
+		print(error)
+	}
+	
+	override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+		if segue.identifier == "toDetails",
+		   let indexPath = sender as? IndexPath,
+		   let destinationVC = segue.destination as? DetailSpotViewController {
+			destinationVC.spotDetails = surfSpots[indexPath.row]
+		}
+	}
+}
 
 //MARK: Handle Data source and delegate of tableview
 extension ViewController: UITableViewDelegate, UITableViewDataSource {
     
     // Sections
-    func numberOfSections(in tableView: UITableView) -> Int {
-        return 3
-    }
-    
-    func tableView(_ tableView: UITableView, titleForHeaderInSection section: Int) -> String? {
-        switch section {
-        case 1:
-            return "Beach Break"
-        case 2:
-            return "Reef Break"
-        default:
-            return "Point Break"
-        }
-    }
+//    func numberOfSections(in tableView: UITableView) -> Int {
+//        return 3
+//    }
+//    
+//    func tableView(_ tableView: UITableView, titleForHeaderInSection section: Int) -> String? {
+//        switch section {
+//        case 1:
+//            return "Beach Break"
+//        case 2:
+//            return "Reef Break"
+//        default:
+//            return "Point Break"
+//        }
+//    }
 
     // Rows
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
         switch section {
-        case 1:
-            return 1
         default:
-            return 3
+				return surfSpots.count
         }
     }
     
@@ -52,7 +84,27 @@ extension ViewController: UITableViewDelegate, UITableViewDataSource {
     
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         let cell = tableView.dequeueReusableCell(withIdentifier: "SpotCell", for: indexPath) as! SpotCell
-        cell.setUpCell()
-        return cell
+		let spotField = surfSpots[indexPath.row]
+		cell.setUpCell(with: spotField)
+		return cell
     }
+	
+	func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+		performSegue(withIdentifier: "toDetails", sender: indexPath)
+	}
 }
+
+extension UIImageView {
+	func load(url: URL) {
+		DispatchQueue.global().async { [weak self] in
+			if let data = try? Data(contentsOf: url) {
+				if let image = UIImage(data: data) {
+					DispatchQueue.main.async {
+						self?.image = image
+					}
+				}
+			}
+		}
+	}
+}
+
