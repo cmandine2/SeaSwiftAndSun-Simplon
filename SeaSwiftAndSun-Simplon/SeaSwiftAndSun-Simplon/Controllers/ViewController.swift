b2e92e18f80fc1e2ee5d725d10f38fe7ae048900
Razafindratsira Ny Dina <nydina.razafindratsira@legrand.com>
2023-12-12 16:49:25 +0100

    Add Section header and filter by surf break

--- SeaSwiftAndSun-Simplon/SeaSwiftAndSun-Simplon/Controllers/ViewController.swift
+++ SeaSwiftAndSun-Simplon/SeaSwiftAndSun-Simplon/Controllers/ViewController.swift
@@ -1,91 +1,68 @@
-//
-//  ViewController.swift
-//  SeaSwiftAndSun-Simplon
-//
-//  Created by Amandine Cousin on 05/12/2023.
-//
-
 import UIKit
-import SwiftUI
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UITableViewDataSource, UITableViewDelegate {
     var spots: [Spot] = []
-        
+    var surfBreakSections: [String] = []
+    var spotsBySurfBreak: [String: [Spot]] = [:]
+
     @IBOutlet weak var tableView: UITableView!
+
     override func viewDidLoad() {
         super.viewDidLoad()
         getSpot()
         self.title = "Liste des spots de surf"
     }
-    
     func getSpot() {
         SpotService.shared.getSpot { error, destination in
-            self.spots = destination?.spots ?? []
+            guard let destination = destination else {
+                return
+            }
+            
+            self.spots = destination.spots
+            
+            // Populate the sections and organize spots by Surf Break
+            self.surfBreakSections = Array(Set(self.spots.map { $0.fields.surfBreak.joined(separator: ", ") }))
+            for surfBreak in self.surfBreakSections {
+                self.spotsBySurfBreak[surfBreak] = self.spots.filter { $0.fields.surfBreak.contains(surfBreak) }
+            }
+            
             self.tableView.reloadData()
-            print("destinations:", destination as Any)
         }
-        
     }
-}
-
-
-//MARK: Handle Data source and delegate of tableview
-extension ViewController: UITableViewDelegate, UITableViewDataSource {
     
-    // Sections
-//    func numberOfSections(in tableView: UITableView) -> Int {
-//        let surfBreak = viewModelDestination.destionations.map { record in
-//            record.fields.surfBreak
-//        }
-//        return surfBreak.count
-//
-//    }
+    func numberOfSections(in tableView: UITableView) -> Int {
+        return surfBreakSections.count
+    }
     
-//    func tableView(_ tableView: UITableView, titleForHeaderInSection section: Int) -> String? {
-//        switch section {
-//        case 1:
-//            return "Beach Break"
-//        case 2:
-//            return "Reef Break"
-//        default:
-//            return "Point Break"
-//        }
-//    }
+    func tableView(_ tableView: UITableView, titleForHeaderInSection section: Int) -> String? {
+        return surfBreakSections[section]
+    }
 
-    // Rows
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-//        switch section {
-//        case 1:
-//            return 1
-//        default:
-//            return 3
-//        }
-        return spots.count
-    }
-    
-    func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
-        return 55
+        let surfBreak = surfBreakSections[section]
+        return spotsBySurfBreak[surfBreak]?.count ?? 0
     }
     
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-       
-        
         let cell = tableView.dequeueReusableCell(withIdentifier: "SpotCell", for: indexPath) as! SpotCell
-        cell.setUpCell(spot: spots[indexPath.row])
+        let surfBreak = surfBreakSections[indexPath.section]
+        if let spotsInSection = spotsBySurfBreak[surfBreak] {
+            cell.setUpCell(spot: spotsInSection[indexPath.row])
+        }
         return cell
     }
     
     override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        if segue.identifier == "SpotDetails" {
-            if let indexPath = self.tableView.indexPathForSelectedRow {
-                let controller = segue.destination as! DetailSpotViewController
-                let selectedSpot = spots[indexPath.row]
-                controller.selectedSpot = selectedSpot
-                guard let cell: SpotCell = tableView.cellForRow(at: indexPath) as? SpotCell else { return }
+        if segue.identifier == "SpotDetails",
+           let indexPath = self.tableView.indexPathForSelectedRow,
+           let controller = segue.destination as? DetailSpotViewController {
+            let selectedSpot = spots[indexPath.row]
+            controller.selectedSpot = selectedSpot
+            
+            if let cell = tableView.cellForRow(at: indexPath) as? SpotCell {
                 controller.selectedImage = cell.spotImage.image
             }
-        
         }
     }
-}
 
+}
