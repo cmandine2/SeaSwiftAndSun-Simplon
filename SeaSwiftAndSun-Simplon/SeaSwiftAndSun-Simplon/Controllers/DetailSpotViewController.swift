29ce128ef90e3f52a2f8bad69324aa8996f056c1
NatCanCode <nathalie.piat1@gmail.com>
2023-12-13 15:47:19 +0100

    Add and connect report url and peak season dates

--- SeaSwiftAndSun-Simplon/SeaSwiftAndSun-Simplon/Controllers/DetailSpotViewController.swift
+++ SeaSwiftAndSun-Simplon/SeaSwiftAndSun-Simplon/Controllers/DetailSpotViewController.swift
@@ -12,8 +12,16 @@ class DetailSpotViewController: UIViewController {
     @IBOutlet weak var spotName: UILabel!
     @IBOutlet weak var spotDestination: UILabel!
     @IBOutlet weak var spotDifficulty: UILabel!
+    @IBOutlet weak var peakSeasonView: UIStackView!
+    @IBOutlet weak var peakSeasonLabelOne: UILabel!
+    @IBOutlet weak var peakSeasonLabelTwo: UILabel!
+    @IBOutlet weak var peakSeasonStartDate: UILabel!
+    @IBOutlet weak var peakSeasonEndDate: UILabel!
+    @IBOutlet weak var magicSinkWeedLink: UIButton!
+    @IBOutlet weak var link: UIStackView!
     var spotDetails: SurfSpotFields?
-    
+    var url = URL(string: "")
+
     override func viewDidLoad() {
         super.viewDidLoad()
         print("details loaded")
@@ -28,7 +36,6 @@ class DetailSpotViewController: UIViewController {
                    let url = URL(string: thumbnailUrl) {
                     if let spotImage = self.spotImage {
                         self.spotImage.load(url: url)
-                        self.spotImage.layer.cornerRadius = self.spotImage.frame.size.width / 2
                     }
                 } else {
                     self.spotImage.image = UIImage(named: "surfSpot") // Image par défaut
@@ -37,6 +44,17 @@ class DetailSpotViewController: UIViewController {
             self.spotName.text = details.destination
             self.spotDestination.text = details.destinationStateCountry
             self.spotDifficulty.text = String(details.difficultyLevel)
+            self.magicSinkWeedLink.setTitle("Magic Seaweed Link", for: .normal)
+            self.peakSeasonLabelOne.text = "Peak season runs from"
+            self.peakSeasonLabelTwo.text = "to"
+            self.peakSeasonStartDate.text = details.peakSurfSeasonBegins
+            self.peakSeasonEndDate.text = details.peakSurfSeasonEnds
+        }
+    }
+
+    @IBAction func goToURL(_ sender: Any) {
+        if let url = URL(string: spotDetails?.magicSeaweedLink ?? "") {
+            UIApplication.shared.open(url)
         }
     }
 }
