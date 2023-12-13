481e4a7cd0a7b85b0f2bfcdce20314c66ddda947
Cynthia-Cyber-Code <cynthiaf181999@gmail.com>
2023-12-13 11:23:25 +0100

    [V1-test]test function loadimage

--- SeaSwiftAndSun-Simplon/SeaSwiftAndSun-SimplonTests/SeaSwiftAndSun_SimplonTests.swift
+++ SeaSwiftAndSun-Simplon/SeaSwiftAndSun-SimplonTests/SeaSwiftAndSun_SimplonTests.swift
@@ -16,15 +16,14 @@ final class SeaSwiftAndSun_SimplonTests: XCTestCase {
     override func tearDownWithError() throws {
         // Put teardown code here. This method is called after the invocation of each test method in the class.
     }
+    func testLoadedImage() async throws {
+        var spotImage = await UIImageView()
+        let imageURL = URL(string: "https://source.unsplash.com/random/300x200")!
+        let imageFetcher: () =  await spotImage.load(url: (imageURL))
 
-    func testExample() throws {
-        // This is an example of a functional test case.
-        // Use XCTAssert and related functions to verify your tests produce the correct results.
-        // Any test you write for XCTest can be annotated as throws and async.
-        // Mark your test throws to produce an unexpected failure when your test encounters an uncaught error.
-        // Mark your test async to allow awaiting for asynchronous code to complete. Check the results with assertions afterwards.
+        XCTAssertNotNil(imageFetcher)
     }
-
+    
     func testPerformanceExample() throws {
         // This is an example of a performance test case.
         measure {
